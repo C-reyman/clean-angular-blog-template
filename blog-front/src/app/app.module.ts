9757b8b2325a5889db0abd50dda3a74836fefb18
C-reyman <carlosreyman2@gmail.com>
2023-07-08 14:43:53 +0200

    inicio

--- blog-front/src/app/app.module.ts
+++ blog-front/src/app/app.module.ts
@@ -4,7 +4,8 @@ import { FormsModule } from "@angular/forms";
 import { AppRoutingModule } from "./app-routing.module";
 import { Http, HttpModule } from "@angular/http";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
--- blog-front/src/app/home/home.component.ts
+++ blog-front/src/app/home/home.component.ts
@@ -1,22 +1,27 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
-
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 import { BlogPostService } from "../service/blog-post.service";
 import { BlogPost } from "../models/blog-post";
 
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.css"]
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
   loading: boolean = true;
   posts: BlogPost[];
-
+  longText: string;
   constructor(private postService: BlogPostService, private router: Router) {}
 
   ngOnInit() {
     this.getPosts();
+    this.loading = false;
+    this.longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+    from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+    originally bred for hunting.`;
   }
 
   private getPosts(): void {
